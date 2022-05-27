a7eab57d9e4e986d399bff2cfb0f48be705f57be
kamrana005 <kamranda@code.edu.az>
2022-05-27 13:35:56 +0400

    new

---
+++ ConsoleApp2home1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp2home1
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int[] listOfNumbers = { 23, 34, 43 };
+            int number = 15;
+            newNumber(listOfNumbers, 15);
+    
+        }
+
+        static void newNumber(int[] listOfNumbers, int number)
+        {
+            int[] newd = new int[listOfNumbers.Length + 1];
+            for (int i = 0; i < listOfNumbers.Length; i++)
+            {
+                newd [i]= listOfNumbers[i];
+            }
+            newd [new.Length - 1] = number;
+        }
+
+
+    }
+    
+
+}
